1435cad73972455edc20eee2880bcd1204457b00
Roberto Obregon <obregonroberto3@gmail.com>
2021-04-07 11:10:50 -0500

    Ya salio la lista

---
+++ Prueba1/Prueba1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Prueba1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        public class Cliente
+        {
+            public string codigo { get; set; }
+            public string name { get; set; }
+            public string ruc { get; set; }
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://api.jsonbin.io/b/606cd11e9c59a9732cae995a");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accpet", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<Cliente>>(content);
+
+                ListDemo.ItemsSource = resultado;
+            }
+
+        }
+
+    }
+}
